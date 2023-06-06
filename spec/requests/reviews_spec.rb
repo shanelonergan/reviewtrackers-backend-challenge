9c2c6ad2224a9dd2ce4c19b509138be0894eb978
shanelonergan <sptlonergan@gmail.com>
2023-06-06 12:20:10 -0400

    setup rspec

---
+++ spec/requests/reviews_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "review API", :type => :request do
+
+  describe "collect_reviews" do
+    it "returns a list of reviews from a valid lendingtree url" do
+    end
+  end
+end
